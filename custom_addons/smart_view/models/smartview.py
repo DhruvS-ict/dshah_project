d3f077a800d291c5f94a70ca20bab130f89018d7
DhruvS-ict <dhruv.shah@icreativetechnolabs.com>
2022-03-04 18:39:24 +0530

    [ADD]smart_view_super class with create and rainbow effect.

--- custom_addons/smart_view/models/smartview.py
+++ custom_addons/smart_view/models/smartview.py
@@ -1,23 +1,24 @@
 """This"""
 from datetime import datetime
-from odoo import models, fields
+from odoo import models, fields, api
 # from odoo.exceptions import ValidationError
 
 
 class SmartView(models.Model):
     """This class is for fields & orm methods."""
     _name = 'smart.view'
-    _inherit = 'mail.thread','mail.activity.mixin'
+    # _inherit = 'mail.thread','mail.activity.mixin'
     _description = "Created this module to display Smart button inside sheet in form view mode."
     name = fields.Many2one('college_management.college_management',string="Name")
-    # phone_no = fields.Char(string="Phone Number", related="name.phone_no")
+    phone_no = fields.Char(string="Phone Number", related="name.phone_no")
     age = fields.Integer(string="Age", tracking=True)
     residential_address = fields.Text(string="Residential Address")
     date = fields.Datetime(string='Date', default=datetime.now())
     mail_id = fields.Char(string="Mail ID")
     dob = fields.Date(string="Date of Birth")
-    cus_one_to_many = fields.One2many('smart.cus','management_id',string="cusonetomany",
-                                      related="name.cus_one_to_many")
+    select = fields.Selection([('male','Male'),('female','Female')],string="Select")
+    # cus_one_to_many = fields.One2many('smart.cus','management_id',string="cusonetomany",
+    #                                   related="name.cus_one_to_many")
 
     # _sql_constraints = [('raiseerror_uniq', 'unique (name)',
     # "This name is already exists! Please enter unique name"),]
@@ -31,11 +32,18 @@ class SmartView(models.Model):
         return self
 
     def create_orm(self):
-        """This function is created for create button."""
+        """This function is created for create button.
+        And also return rainbow effect."""
         vals = {'name': 'Raj', 'age': '34', 'residential_address': 'Gujarat', 'create_uid': '3'}
         # self.message_post(body="xyz has created recent record.")
         self.env['smart.view'].create(vals)
-        return self
+        return {
+            'effect':{
+                'fadeout':'fast',
+                'message':'Record created successfully',
+                'type':'rainbow_man',
+            }
+        }
 
     def write_orm(self):
         """This function is created for write button."""
@@ -48,12 +56,27 @@ class SmartView(models.Model):
         self.env['smart.view'].browse(108)
         return self
 
-class CollegeManagementCus(models.Model):
-    """This class is created for one to many field."""
-    _name = 'smart.cus'
-    cus_id = fields.Many2one('res.partner', string="otm_id")
-    n_name = fields.Char(string="Naming")
-    management_id = fields.Many2one('college_management.college_management', string="management_id")
+    @api.model
+    def create(self,vals):
+        """This function is created for calling Super."""
+        res = super(SmartView, self).create(vals)
+        if vals.get('select') == 'male':
+            res['name'] = "Mr."+res['name']
+        elif vals.get('select') == 'female':
+            res['name'] = "Mrs."+res['name']
+        return res
+
+
+
+
+
+# class CollegeManagementCus(models.Model):
+#     """This class is created for one to many field."""
+#     _name = 'smart.cus'
+#     cus_id = fields.Many2one('res.partner', string="otm_id")
+#     n_name = fields.Char(string="Naming")
+#     management_id = fields.Many2one('college_management.college_management',
+#     string="management_id")
 
     # @api.constrains('age')
     # def check_age(self):
